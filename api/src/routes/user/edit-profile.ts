c31b0d363a28a770b585cef49d946b33d2992b96
Carlos Faustino <chfcchfc96@gmail.com>
2025-06-18 11:21:36 -0300

    feat: add edit profile route

---
+++ api/src/routes/user/edit-profile.ts
@@ -0,0 +1,79 @@
+import { compare, hash } from "bcryptjs"
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
+import { z } from "zod"
+
+import { ClientError } from "@/errors/client-error"
+import { prisma } from "@/lib/prisma"
+import { auth } from "@/middlewares/auth"
+import { PASSWORD_REGEX } from "@/utils/constants"
+
+export const editProfile: FastifyPluginAsyncZod = async app => {
+  app.register(auth).patch(
+    "/profile",
+    {
+      schema: {
+        body: z
+          .object({
+            name: z.string().optional(),
+            email: z.string().email().optional(),
+            phone: z.string().optional(),
+            oldPassword: z.string().optional(),
+            password: z
+              .string()
+              .min(8)
+              .refine(value => PASSWORD_REGEX.test(value), {
+                message:
+                  "Password must contain at least eight characters, an uppercase letter, a lowercase letter, a number and a special character",
+              })
+              .optional(),
+            confirmPassword: z.string().optional(),
+          })
+          .refine(data => data.password === data.confirmPassword, {
+            message: "Password and confirm password don't match",
+            path: ["confirmPassword"],
+          }),
+      },
+    },
+    async (request, reply) => {
+      const userId = await request.getCurrentUserId()
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      })
+
+      if (!user) {
+        throw new ClientError("User not found")
+      }
+
+      const { name, password, phone, email, oldPassword } = request.body
+
+      if (oldPassword && !(await compare(oldPassword, user.password))) {
+        throw new ClientError(
+          "New password cannot be equal to the old password",
+        )
+      }
+
+      let hashedPassword
+
+      if (password) {
+        hashedPassword = await hash(password, 10)
+      }
+
+      await prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          name,
+          password: hashedPassword,
+          phone,
+          email,
+        },
+      })
+
+      return reply.send()
+    },
+  )
+}
