52ba3ebde508e625c724d12d6faa3715c4597ab7
Carlos Faustino <chfcchfc96@gmail.com>
2025-06-25 13:19:21 -0300

    feat:  filter restaurants

--- api/src/routes/restaurant/list-restaurants.ts
+++ api/src/routes/restaurant/list-restaurants.ts
@@ -1,14 +1,22 @@
+import { TZDate } from "@date-fns/tz"
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
-import { Prisma } from "generated/prisma"
 import { z } from "zod"
 
 import { prisma } from "@/lib/prisma"
+import { restaurantIsOpen } from "@/utils/check-restaurant-is-open"
+import { convertMinutesToHours } from "@/utils/convert-minutes-to-hours"
 
 export const listRestaurants: FastifyPluginAsyncZod = async app => {
   app.get(
     "/restaurant",
     {
       schema: {
+        querystring: z.object({
+          category: z.string().optional(),
+          tax: z.coerce.number().optional(),
+          deliveryTime: z.coerce.number().optional(),
+          pageIndex: z.coerce.number().default(0),
+        }),
         response: {
           200: z.object({
             restaurants: z.array(
@@ -16,52 +24,91 @@ export const listRestaurants: FastifyPluginAsyncZod = async app => {
                 id: z.string().uuid(),
                 name: z.string(),
                 phone: z.string(),
-                tax: z.custom<Prisma.Decimal>(),
+                tax: z.number(),
                 deliveryTime: z.number(),
                 image: z.string().nullable(),
                 adminId: z.string().uuid(),
-                hours: z.array(
-                  z.object({
-                    id: z.string().uuid(),
-                    weekday: z.number(),
-                    openedAt: z.number(),
-                    closedAt: z.number(),
-                    open: z.boolean(),
-                    restaurantId: z.string().uuid(),
-                  }),
-                ),
                 category: z.object({
                   id: z.string().uuid(),
                   name: z.string(),
                 }),
+                isOpen: z.boolean(),
+                openingAt: z.string().optional(),
               }),
             ),
+            meta: z.object({
+              totalCount: z.number(),
+              pageIndex: z.number(),
+              perPage: z.number(),
+            }),
           }),
         },
       },
     },
-    async () => {
+    async request => {
+      const { category, tax, deliveryTime, pageIndex } = request.query
+
       const restaurants = await prisma.restaurant.findMany({
         include: {
           hours: {
             orderBy: [{ weekday: "asc" }],
+            where: {
+              weekday: new TZDate().getDay(),
+              open: true,
+            },
           },
           category: true,
+        },
+        where: {
+          category: {
+            name: {
+              mode: "insensitive",
+              equals: category,
+            },
+          },
+          tax: {
+            lte: tax,
+          },
+          deliveryTime: {
+            lte: deliveryTime,
+          },
+        },
+        take: 10,
+        skip: pageIndex * 10,
+      })
+
+      const totalCount = await prisma.restaurant.count({
+        where: {
+          category: {
+            name: {
+              mode: "insensitive",
+              equals: category,
+            },
+          },
+          tax: {
+            lte: tax,
+          },
+          deliveryTime: {
+            lte: deliveryTime,
+          },
         },
       })
 
       return {
         restaurants: restaurants.map(item => ({
-          id: item.id,
-          name: item.name,
-          adminId: item.adminId,
-          phone: item.phone,
-          tax: item.tax,
-          deliveryTime: item.deliveryTime,
-          image: item.image,
-          hours: item.hours,
-          category: item.category,
+          ...item,
+          tax: item.tax.toNumber(),
+          isOpen: restaurantIsOpen(item.hours[0]),
+          openingAt:
+            item.hours[0].open && !restaurantIsOpen(item.hours[0])
+              ? convertMinutesToHours(item.hours[0].openedAt)
+              : undefined,
         })),
+        meta: {
+          totalCount,
+          pageIndex,
+          perPage: 10,
+        },
       }
     },
   )
