b869459a2d21a0882a5b962fe0028516941fbd4b
Carlos Faustino <chfcchfc96@gmail.com>
2025-06-30 09:12:24 -0300

    feat: add shadcn-ui

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,17 @@
+import { resolve } from "node:path"
+
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 })
