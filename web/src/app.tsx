8111f7f99baf50a6e7cc7c8cff440f210dccffe5
Carlos Faustino <chfcchfc96@gmail.com>
2025-06-30 12:34:40 -0300

    feat: add env validation, axios, react-query and sonner

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,12 +1,18 @@
+import { QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider } from "react-router"
+import { Toaster } from "sonner"
 
 import { ThemeProvider } from "./components/theme/theme-provider"
+import { queryClient } from "./lib/react-query"
 import { router } from "./pages/routes"
 
 export function App() {
   return (
     <ThemeProvider storageKey="@food-app-theme">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+      <Toaster richColors />
     </ThemeProvider>
   )
 }
