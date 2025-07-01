8f49ea1fbc76d207103ad00030c784117e78e2d3
Carlos Faustino <chfcchfc96@gmail.com>
2025-07-01 12:46:40 -0300

    refactor: update react query

--- web/src/pages/auth/sign-up.tsx
+++ web/src/pages/auth/sign-up.tsx
@@ -65,7 +65,6 @@ export function SignUp() {
   const { data: categories } = useQuery({
     queryKey: ["categories"],
     queryFn: getCategories,
-    refetchOnWindowFocus: false,
   })
 
   const { mutateAsync: signUpFn } = useMutation({
