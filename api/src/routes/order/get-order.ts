13df71ca165c705d46f00aae2a7bc7bd1e202436
Carlos Faustino <chfcchfc96@gmail.com>
2025-07-02 11:14:02 -0300

    feat: update response

--- api/src/routes/order/get-order.ts
+++ api/src/routes/order/get-order.ts
@@ -5,61 +5,60 @@ import { z } from "zod"
 import { ClientError } from "@/errors/client-error"
 import { prisma } from "@/lib/prisma"
 import { auth } from "@/middlewares/auth"
-import { verifyUserRole } from "@/middlewares/verify-user-role"
 
 export const getOrder: FastifyPluginAsyncZod = async app => {
   app.register(auth).get(
     "/order/:orderId",
     {
-      preHandler: [verifyUserRole("CLIENT")],
       schema: {
         params: z.object({
           orderId: z.string().uuid(),
         }),
         response: {
           200: z.object({
-            order: z.object({
-              id: z.string().uuid(),
-              date: z.date(),
-              payment: z.nativeEnum(PaymentMethod),
-              status: z.nativeEnum(OrderStatus),
-              total: z.number(),
-              grade: z.number().nullable(),
-              address: z.object({
-                address: z.string(),
-                number: z.number().nullable(),
-                district: z.string(),
-                city: z.string(),
-                uf: z.string(),
-              }),
-              restaurant: z.object({
-                name: z.string(),
-                image: z.string().nullable(),
-                deliveryTime: z.number(),
-                tax: z.number(),
-              }),
-              products: z.array(
-                z.object({
-                  id: z.string().uuid(),
-                  name: z.string(),
-                  price: z.number(),
-                  quantity: z.number(),
-                  image: z.string().nullable(),
-                }),
-              ),
+            id: z.string().uuid(),
+            date: z.date(),
+            payment: z.nativeEnum(PaymentMethod),
+            status: z.nativeEnum(OrderStatus),
+            total: z.number(),
+            grade: z.number().nullable(),
+            client: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              phone: z.string(),
             }),
+            address: z.object({
+              address: z.string(),
+              number: z.number().nullable(),
+              district: z.string(),
+              city: z.string(),
+              uf: z.string(),
+            }),
+            restaurant: z.object({
+              name: z.string(),
+              image: z.string().nullable(),
+              deliveryTime: z.number(),
+              tax: z.number(),
+            }),
+            products: z.array(
+              z.object({
+                id: z.string().uuid(),
+                name: z.string(),
+                price: z.number(),
+                quantity: z.number(),
+                image: z.string().nullable(),
+              }),
+            ),
           }),
         },
       },
     },
     async request => {
-      const clientId = await request.getCurrentUserId()
       const { orderId } = request.params
 
       const order = await prisma.order.findUnique({
         where: {
           id: orderId,
-          clientId,
         },
         select: {
           id: true,
@@ -69,6 +68,13 @@ export const getOrder: FastifyPluginAsyncZod = async app => {
           status: true,
           total: true,
           grade: true,
+          client: {
+            select: {
+              name: true,
+              phone: true,
+              email: true,
+            },
+          },
           address: {
             select: {
               address: true,
@@ -102,26 +108,24 @@ export const getOrder: FastifyPluginAsyncZod = async app => {
         },
       })
 
-      if (!order || order.clientId !== clientId) {
-        throw new ClientError("Order not found or order not belongs to user")
+      if (!order) {
+        throw new ClientError("Order not found")
       }
 
       return {
-        order: {
-          ...order,
-          total: order.total.toNumber(),
-          restaurant: {
-            ...order.restaurant,
-            tax: order.restaurant.tax.toNumber(),
-          },
-          products: order.orderItems.map(item => ({
-            id: item.id,
-            name: item.product.name,
-            price: item.price.toNumber() * item.quantity,
-            quantity: item.quantity,
-            image: item.product.image,
-          })),
+        ...order,
+        total: order.total.toNumber(),
+        restaurant: {
+          ...order.restaurant,
+          tax: order.restaurant.tax.toNumber(),
         },
+        products: order.orderItems.map(item => ({
+          id: item.id,
+          name: item.product.name,
+          price: item.price.toNumber(),
+          quantity: item.quantity,
+          image: item.product.image,
+        })),
       }
     },
   )
