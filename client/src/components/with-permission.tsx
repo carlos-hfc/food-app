3e527f6329011466e45c166b9a39a76828abbfdc
Carlos Faustino <chfcchfc96@gmail.com>
2025-08-07 16:17:35 -0300

    feat: intercept response when user not logged

--- client/src/components/with-permission.tsx
+++ client/src/components/with-permission.tsx
@@ -1,15 +1,43 @@
-import { ComponentType } from "react"
-import { Navigate, useLocation } from "react-router"
+import { isAxiosError } from "axios"
+import { ComponentType, useEffect } from "react"
+import { Navigate, useLocation, useNavigate } from "react-router"
+
+import { api } from "@/lib/axios"
 
 export function withPermission<T extends object>(Component: ComponentType<T>) {
   const ComponentWithNavigation = (props: T) => {
     const { pathname } = useLocation()
+    const navigate = useNavigate()
     const logged = sessionStorage.getItem("isLogged")
+    const redirectOnError = `/sign-in?redirectTo=${pathname}`
+
+    useEffect(() => {
+      const interceptorId = api.interceptors.response.use(
+        response => response,
+        error => {
+          if (isAxiosError(error)) {
+            const message = error.response?.data.message
+
+            if (message === "Invalid auth token") {
+              sessionStorage.removeItem("isLogged")
+              navigate(redirectOnError, { replace: true })
+            }
+          }
+        },
+      )
+
+      return () => {
+        api.interceptors.response.eject(interceptorId)
+      }
+    }, [navigate, redirectOnError])
 
     return logged ? (
       <Component {...props} />
     ) : (
-      <Navigate to={`/sign-in?redirectTo=${pathname}`} />
+      <Navigate
+        to={redirectOnError}
+        replace
+      />
     )
   }
 
