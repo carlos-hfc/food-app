a6247606e9ad560d987ed87c0f5468953710e8a1
Carlos Faustino <chfcchfc96@gmail.com>
2025-08-15 14:18:29 -0300

    test: update list categories test

--- api/src/routes/category/list-categories.spec.ts
+++ api/src/routes/category/list-categories.spec.ts
@@ -1,35 +1,53 @@
+import { faker } from "@faker-js/faker"
 import request from "supertest"
 import { afterAll, beforeAll, describe, expect, it } from "vitest"
 
 import { prisma } from "@/lib/prisma"
 import { app } from "@/server"
 
+const categoryName = faker.word.noun()
+
 describe("List categories [GET] /categories", () => {
   beforeAll(async () => {
     await app.ready()
-    await prisma.category.createManyAndReturn({
-      data: [{ name: "Brasileira" }, { name: "Pizza" }, { name: "Lanches" }],
-    })
   })
 
   afterAll(async () => {
-    await prisma.category.deleteMany()
     await app.close()
   })
 
   it("should be able to list categories", async () => {
+    const category = await prisma.category.create({
+      data: { name: categoryName },
+    })
+
     const response = await request(app.server).get("/categories").send()
 
     expect(response.status).toEqual(200)
-    expect(response.body).toHaveLength(3)
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: category.id,
+          name: categoryName,
+        }),
+      ]),
+    )
   })
 
   it("should be able to list categories filtering by name", async () => {
     const response = await request(app.server)
       .get("/categories")
-      .query({ name: "Brasileira" })
+      .query({ name: categoryName })
 
     expect(response.status).toEqual(200)
     expect(response.body).toHaveLength(1)
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          name: categoryName,
+        }),
+      ]),
+    )
   })
 })
